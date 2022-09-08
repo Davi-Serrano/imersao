af90303cd3ca6c3d7170da93ddffd301344e7821
Davi Serrano <daviscardoso2901@gmail.com>
2022-09-08 20:21:53 -0300

    versão responsiva da pagina de finalização de cadastro

--- src/pages/teste.tsx
+++ src/pages/teste.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Center, Flex, Text } from "@chakra-ui/react";
 import { SiWhatsapp } from "react-icons/si"
 export default function Home() {
   return (
@@ -8,20 +8,28 @@ export default function Home() {
       flexDir="column"
       bgGradient='linear(45deg , black, #8B0000)'
       minH="100vh" 
-      fontSize="2em"
+      fontSize={["26px", "2em"]}
     >
-      <Flex>
-        Seu cadastro está <Text  px=".2em" color="red">  80%  </Text>concluido.
+      <Flex flexDir={["column", "row"]} justify="center" align="center">
+        Seu cadastro está <Text color="red">80%</Text> concluido.
       </Flex> 
-      <Flex fontSize="18px">
+      <Flex textAlign="center" fontSize="18px" m="2em">
         Entre no grupo do Whatsapp para não perder nenhuma novidade.
       </Flex> 
 
-      <Flex borderRadius={32}   p=".5em" my=".5em" bg="#D35000" align="center" justify="space-around" _hover={{
-        cursor: "pointer",
-        opacity: .8
-      }}>
-        <SiWhatsapp color="#39ff14" /> <Flex pl="1em"> Entrar no grupo do Whatsapp.</Flex>
+      <Flex 
+          borderRadius={32}   
+          p=".5em" my=".5em" 
+          bg="#D35000" 
+          align="center" 
+          justify="space-around"
+          maxW="80vw" 
+          _hover={{
+            cursor: "pointer",
+            opacity: .8
+          }}
+        >
+        <SiWhatsapp color="#39ff14" size={60} /> <Flex pl="1em"> Entrar no grupo do Whatsapp.</Flex>
       </Flex> 
       <Flex>
       
